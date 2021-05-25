d12d6639603902a1d3744db7e7cca571e54899a4
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2021-05-25 15:26:05 +0530

    Updated

--- Image and Video/Read_Image_Video.py
+++ Image and Video/Read_Image_Video.py
@@ -6,19 +6,23 @@ img = cv2.imread("./Resources/lena.png")
 cv2.imshow("Lena", img)
 cv2.waitKey(1000)
 
+# Parameter for Video-sizing
 frameWidth = 640
 frameHeight = 480
 
 # Reading video
 cap = cv2.VideoCapture(0)
-# cap.set(3, frameWidth)
-# cap.set(4, frameHeight)
 
-# Displaying the image and video
+# Resize video window
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+
+# Displaying the image or video
 while True:
     success, img = cap.read()
-    # img = cv2.resize(img, (frameWidth, frameHeight))
     cv2.imshow("Cam", img)
-
+    
+    # img = cv2.resize(img, (frameWidth, frameHeight))
+    # cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
