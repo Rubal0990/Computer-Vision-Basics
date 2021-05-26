4b100ebc4851c1b76d18cba7e95cd20a431a9974
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2021-05-26 22:58:53 +0530

    Updated

--- Detecting mouse click/Detect_Click.py
+++ Detecting mouse click/Detect_Click.py
@@ -1,13 +1,17 @@
+# Importing Library
 import cv2
 
-
+# Function to detect click
 def mousePoints(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         print(x, y)
 
-
+# Reading Image
 img = cv2.imread('Resources/chess.jpg')
 
+# Displaying Original Image
 cv2.imshow("Original Image ", img)
+
+# Function Call to detect click
 cv2.setMouseCallback("Original Image ", mousePoints)
 cv2.waitKey(0)
