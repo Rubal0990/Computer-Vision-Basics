9b4fe13cf9735eefff4fe94aa71a641c6aecd274
Rubal Bansal <56014427+Rubal0990@users.noreply.github.com>
2021-05-26 23:05:08 +0530

    Updated

--- Wrap Prespective/wrap_prespective.py
+++ Wrap Prespective/wrap_prespective.py
@@ -1,17 +1,28 @@
+# Importing Libraries
 import cv2
 import numpy as np
 
+# Reading Image
 img = cv2.imread('Resources/chess.jpg')
 
+# Parameters
 width, height = 250, 350
+
+# Points for Wrap Prespective
 pts1 = np.float32([[204, 200], [278, 169], [266, 238], [342, 203]])
 pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+
+# Creating the Bird-eye view of Image
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
 imgOutput = cv2.warpPerspective(img, matrix, (width, height))
 
+# Drawing Circles on Original Image
 for x in range(0, 4):
     cv2.circle(img, (pts1[x][0], pts1[x][1]), 15, (0, 255, 0), cv2.FILLED)
 
+# Displaying the Original Image
 cv2.imshow("Original Image", img)
+
+# Displaying the Bird-eye view of Original Image
 cv2.imshow("Output Image", imgOutput)
 cv2.waitKey(0)
